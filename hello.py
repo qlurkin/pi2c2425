e7be1122f0b3011fa883a0bb1399737cc52830bc
Quentin Lurkin <qlurkin@gmail.com>
2025-02-11 14:15:45 +0100

    Fix Test

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-with open('config.secret') as file:
-    secret = file.read()
+# with open('config.secret') as file:
+#     secret = file.read()
 
 print("hello main")
 print("hello yop")
 print("hahahahaha")
-print(secret)
+# print(secret)
 print(np.zeros(10))
 
 def add(a: float, b: float) -> float:
